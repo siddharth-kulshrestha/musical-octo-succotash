44e66f2090b3c49670f1c561da4b13bc5f964b89
Ayush Verma <ayush_verma@srmuniv.edu.in>
2019-04-11 00:44:11 +0530

    Add files via upload

---
+++ Configurations/constraints.js
@@ -0,0 +1,5 @@
+module.exports = Object.freeze({
+    PROBLEM_URL: 'https://github.com/',
+    CONCURRENT_REQUESTS: 5
+
+});
---
+++ index.js
@@ -0,0 +1,77 @@
+const request = require('request');
+const constraints = require('./Configurations/constraints');
+const is_formatted_url = require('./Utlities/utils');
+const _ = require('lodash/core');
+const cheerio = require('cheerio');
+const URL = require('url-parse');
+const fs = require('fs');
+
+const HYPERLINKS_HARVESTED = {};
+const POSSIBLE_HYPERLINKS = [];
+const url = new URL(constraints.PROBLEM_URL);
+
+console.log(is_formatted_url);
+if ( is_formatted_url === true ) {
+    root_url_value = url => {
+        `{$url.protocol}"//"{$url.hostname}`   // "https://medium.com"
+    };
+
+} else console.log(`Error ${root_url_value} is not an URL`);
+
+let count_of_already_visited_links = 0;
+
+POSSIBLE_HYPERLINKS.unshift(constraints.PROBLEM_URL);
+
+complete_unique_list();
+
+function complete_unique_list() {
+    if (count_of_already_visited_links >= constraints.CONCURRENT_REQUESTS) {
+        console.log("Reached max limit of number of pages to visit.");
+        return;
+    }
+
+    var nextPage = POSSIBLE_HYPERLINKS.pop();
+    if (nextPage in HYPERLINKS_HARVESTED) {
+        // We've already visited this page, so repeat the complete_unique_list
+        complete_unique_list();
+    } else {
+        // New page we haven't visited
+        visitPage(nextPage, complete_unique_list);
+    }
+}
+
+function visitPage(url, callback) {
+    // Add page to our set
+    HYPERLINKS_HARVESTED[url] = true;
+    count_of_already_visited_links++;
+
+    // Make the request
+    console.log("Visiting page " + url);
+    request(url, function (error, response, body) {
+        // Check status code (200 is HTTP OK)
+        console.log("Status code: " + response.statusCode);
+        if (response.statusCode !== 200) {
+            callback();
+            return;
+        }
+        var $ = cheerio.load(body);
+        fetchingLinks($);
+        // In this short program, our callback is just calling complete_unique_list()
+        callback();
+    });
+}
+
+function fetchingLinks($) {
+    var relativeLinks = $("a[href^='/']");
+    var absoluteLinks = $("a[href^='http']");
+    console.log("Found " + relativeLinks.length + " relative links on page");
+    console.log("Found " + absoluteLinks.length + "absolute links on page");
+    relativeLinks.each(function () {
+        POSSIBLE_HYPERLINKS.push(root_url_value + $(this).attr('href'));
+        fs.appendFileSync('C:\\Users\\Mohit k\\WebstormProjects\\rentomojoFile.csv', (root_url_value + $(this).attr('href')) + '\n');
+    });
+    absoluteLinks.each(function () {
+        POSSIBLE_HYPERLINKS.push($(this).attr('href'));
+        fs.appendFileSync('C:\\Users\\Mohit k\\WebstormProjects\\rentomojoFile.csv', ($(this).attr('href')) + '\n');
+    });
+}
